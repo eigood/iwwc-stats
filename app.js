3ef8c45e1e06b25c3e1e76da5d0715e277d50c7f
Adam Heath <doogie@brainfood.com>
2023-11-16 23:35:50 -0600

    Now that the class is correct, call the correct method to attach it.

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ async function fetchJSON(url, handler) {
 function handleLoad() {
   //fetchText(ghPagesBase + '/app.html', setHtml);
   loadData();
-  document.querySelector('.reload-button').addEventHandler('click', loadData)
+  document.querySelector('.reload-button').addListener('click', loadData)
 }
 
 function loadData(e) {
