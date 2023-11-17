0191149dd528d79dac004d456c58967a97cceda0
Adam Heath <doogie@brainfood.com>
2023-11-16 21:20:25 -0600

    This might be it, very close.

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var iwwcCustomURL = 'https://eigood.github.io/iwwc-stats-data/iwwc-custom.json';
+const skipStats = {
+  'ap': true,
+  'level': true,
+  'faction': true,
+  'last_submit': true,
+}
 
 async function fetchJSON(url, handler) {
   const response = await fetch(url, {mode: 'no-cors'})
@@ -19,26 +25,51 @@ function loadData() {
 function handleData(iwwcData) {
   console.log('checkApp', {iwwcData: iwwcData});
   if (!iwwcData) return;
+  const app = document.querySelector('#iwwc-app')
+  app.className = ''
+
   var statPaneTemplate = document.querySelector('#stat-pane');
   var statListRowTemplate = document.querySelector('#stat-list-row');
   console.log('templates', { statPaneTemplate, statListRowTemplate });
   const byAgent = {}
   const byStat = {};
+
   Object.entries(iwwcData).forEach(([ agentName, agentData ]) => {
     const forAgent = byAgent[ agentName ] = {}
     Object.entries(agentData).forEach(([ statName, statValue ]) => {
+      if (skipStats[ statName ]) return
       forAgent[ statName ] = [ statValue ]
       const statList = byStat[ statName ] || (byStat[ statName ] = [])
       statList.push([ statValue, agentName ])
     })
   })
   const statSorter = (a, b) => a[0] - b[0]
-  Object.entries(byStat).forEach(([ statName, statList ]) => {
+  const statEntries = Object.entries(byStat)
+  statEntries.forEach(([ statName, statList ]) => {
     statList.sort(statSorter)
     statList.forEach(([ statValue, agentName ], index) => {
       byAgent[ agentName ][ statName ][ 1 ] = index
     })
   })
   console.log('by', {byAgent, byStat})
+  statEntries.sort((a, b) => a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0)
+  const appContentNode = document.querySelector('#iwwc-app .iwwc-content')
+  appContentNode.innerHtml = '';
+  statEntries.forEach(([ statName, statList ]) => {
+    const newStatPaneNode = statPaneTemplate.cloneNode()
+    newStatPaneNode.querySelector('.stat-header').textContent = statName
+    const newStatListNode = newStatPaneNode.querySelector('.stat-list')
+    statList.forEach(([ statValue, agentName ]) => {
+      const newStatRowNode = statListRowTemplate.cloneNode()
+      newStatRowNode.setAttribute('data-value', statValue)
+      const agentInfo = iwwcData[ agentName ]
+      newStatRowNode.querySelector('.stat-value').textContent(statValue)
+      const agentNode = newStatRowNode.querySelector('.agent')
+      agentNode.className += ' faction-' + agentInfo.faction
+      agentNode.textContent = agentName
+      newStatListNode.appendChild(newStatRowNode)
+    })
+    appContentNnode.appendChild(newStatPaneNode)
+  })
 }
 window.addEventListener('load', handleLoad);
