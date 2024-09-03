a4d2be07187504ec807445d7c53a15b89c94fe98
Adam Heath <doogie@brainfood.com>
2024-09-03 12:38:17 -0500

    Add script to run the site locally, using jekyll.

---
+++ run-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+HOST_PORT=4000
+
+# Hard-code this to 4.2.0, as latest doesn't have webrick installed, and fails
+# https://github.com/github/pages-gem/issues/752
+JEKYLL_VERSION=4.2.0
+
+exec docker run \
+	--rm \
+	-ti \
+	--volume="$PWD:/srv/jekyll:Z" \
+	--publish [::1]:$HOST_PORT:4000 \
+	jekyll/jekyll:$JEKYLL_VERSION \
+	jekyll serve
+
