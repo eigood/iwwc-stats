bb0d63b7bb204ce7fdf3fd2a0376382a83824544
Adam Heath <doogie@brainfood.com>
2025-07-06 17:39:26 -0500

    Fix run-docker to use the correct TOP_DIR/PWD.

--- run-docker.sh
+++ run-docker.sh
@@ -4,6 +4,8 @@ set -e
 
 HOST_PORT=4000
 
+TOP_DIR="$(cd "$(dirname "$0")"; pwd -P)"
+
 # Hard-code this to 4.2.0, as latest doesn't have webrick installed, and fails
 # https://github.com/github/pages-gem/issues/752
 JEKYLL_VERSION=4.2.0
@@ -11,7 +13,7 @@ JEKYLL_VERSION=4.2.0
 exec docker run \
 	--rm \
 	-ti \
-	--volume="$PWD:/srv/jekyll:Z" \
+	--volume="$TOP_DIR:/srv/jekyll:Z" \
 	--publish [::1]:$HOST_PORT:4000 \
 	jekyll/jekyll:$JEKYLL_VERSION \
 	jekyll serve --incremental
